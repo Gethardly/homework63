98ff01cbcb260eb1518c80565fbbcd559c68ab96
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2022-12-03 03:37:35 +0600

    Static components for example

---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contacts = () => {
+  return (
+    <div>
+      <h2>Phone: +999 9999 9999 999</h2>
+      <h4>E-mail: Loremipsumdolorsitametconsecteturadipisicingelit@gmail.com</h4>
+      <h6>Address: Lorem ipsum dolor sit amet, consectetur.</h6>
+    </div>
+  );
+};
+
+export default Contacts;
